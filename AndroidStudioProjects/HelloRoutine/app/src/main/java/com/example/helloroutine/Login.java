7db4419ced2b8f59302157d0ff249c64e3d57c94
LEE Junho <cse05214@naver.com>
2021-10-10 22:45:46 +0900

    카카오 로그인 추가

--- AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/Login.java
+++ AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/Login.java
@@ -1,15 +1,46 @@
 package com.example.helloroutine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.kakao.auth.ApiErrorCode;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
 public class Login extends AppCompatActivity {
-    Button btnLogin, btnRegister;
+
+    EditText edtID, edtPw;
+    Button btnLogin, btnRegister, btnKakao;
+    CheckBox checkBox;
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+    SignInButton signInButton;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor, editor2;
+    private boolean saveID;
+    String sID;
+    private SessionCallback sessionCallback;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +49,14 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+        checkBox = findViewById(R.id.saveLogin);
+        btnKakao = findViewById(R.id.btnKakao);
+
+        //카카오 로그인 콜백 초기화
+        sessionCallback = new SessionCallback();
+        Session.getCurrentSession().addCallback(sessionCallback);
+        //앱 실행 시 로그인 토큰이 있으면 자동으로 로그인 수행
+        //Session.getCurrentSession().checkAndImplicitOpen();
 
         //로그인 버튼
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +75,70 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //카카오 로그인
+        btnKakao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Session.getCurrentSession().open(AuthType.KAKAO_LOGIN_ALL, Login.this);
+            }
+        });
     }
+
+    //카카오 로그인
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(sessionCallback);
+    }
+
+    private class SessionCallback implements ISessionCallback {
+        @Override
+        public void onSessionOpened() {
+            UserManagement.getInstance().me(new MeV2ResponseCallback() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    int result = errorResult.getErrorCode();
+
+                    if(result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                        Toast.makeText(getApplicationContext(), "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"로그인 도중 오류가 발생했습니다: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                    Toast.makeText(getApplicationContext(),"세션이 닫혔습니다. 다시 시도해 주세요: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onSuccess(MeV2Response result) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("name", result.getNickname());
+
+                    if(result.getKakaoAccount().hasEmail() == OptionalBoolean.TRUE)
+                        intent.putExtra("email", result.getKakaoAccount().getEmail());
+                    else
+                        intent.putExtra("email", "none");
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException e) {
+            Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: "+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) { //카카오 로그인 액티비티에서 넘어온 경우일 때 실행
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+    }
+
 }
--- AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/Register.java
+++ AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/Register.java
@@ -1,17 +1,261 @@
 package com.example.helloroutine;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Register extends AppCompatActivity {
 
+    TextView txtError1, txtError2, txtError3, txtError4;
+    EditText edtID, edtPw, edtPW2, pickEmail, edtNick;
+    Button btOk, btCheckId;
+    String[] items = {"선택","naver.com","daum.net","nate.com","yahoo.com","hanmail.com","직접입력"}; //이메일 선택
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         setContentView(R.layout.register);
+
+        txtError1 = findViewById(R.id.txtError1);
+        txtError2 = findViewById(R.id.txtError2);
+        txtError3 = findViewById(R.id.txtError3);
+        txtError4 = findViewById(R.id.txtError4);
+        edtID = findViewById(R.id.edtID);
+        edtPw = findViewById(R.id.edtPW);
+        edtPW2 = findViewById(R.id.edtPW2);
+        edtNick = findViewById(R.id.edtNick);
+        pickEmail = findViewById(R.id.edtEmail);
+        btOk = findViewById(R.id.btnOk);
+        btCheckId = findViewById(R.id.btnCheckID);
+        firebaseAuth = FirebaseAuth.getInstance();
+        Spinner spinner = findViewById(R.id.spinner);
+
+        //이메일 선택 스피너
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, items
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                pickEmail.setText(items[i]);
+                pickEmail.setClickable(false);
+                pickEmail.setFocusable(false);
+
+                if(i==6){
+                    pickEmail.setFocusableInTouchMode(true);
+                    pickEmail.setFocusable(true);
+                    pickEmail.setText(null);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //아이디 중복확인(계정 생성 후 로그아웃)
+        btCheckId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String id = edtID.getText().toString().trim()+"@"+pickEmail.getText().toString().trim();
+                final String adminPW = "adminPW";
+                firebaseAuth.createUserWithEmailAndPassword(id, adminPW).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            txtError1.setText("사용 가능한 아이디입니다.");
+                            txtError1.setTextColor(Color.parseColor("#4CAF50"));
+                            edtID.setBackgroundResource(R.drawable.white_edittext);  //테투리 흰색으로 변경
+                            firebaseAuth.signOut();
+                        }
+                        else {
+                            txtError1.setText("이미 등록된 아이디입니다.");
+                            txtError1.setTextColor(Color.RED);
+                            edtID.setBackgroundResource(R.drawable.red_edittext);  //테투리 흰색으로 변경
+
+                        }
+                    }
+                });
+            }
+        });
+
+        //닉네입 입력 중복 시
+        edtNick.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                String nick = edtNick.getText().toString();
+                if (nick.length()>0) {
+                    txtError4.setText("이미 존재한 닉네임입니다.");
+                    txtError4.setTextColor(Color.RED);
+                    edtNick.setBackgroundResource(R.drawable.red_edittext);  //테투리 흰색으로 변경
+                }
+                else{
+                    txtError4.setText("사용할 수 있습니다.");    // 경고 메세지
+                    txtError4.setTextColor(Color.parseColor("#4CAF50"));
+                    edtNick.setBackgroundResource(R.drawable.white_edittext);  // 적색 테두리 적용
+                }
+            }
+        });
+
+
+        //비밀번호 입력 오류 시
+        edtPw.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (edtPw.length()>5) {
+                    txtError2.setText("사용할 수 있습니다.");
+                    txtError2.setTextColor(Color.parseColor("#4CAF50"));
+                    edtPw.setBackgroundResource(R.drawable.white_edittext);  //테투리 흰색으로 변경
+                }
+                else{
+                    txtError2.setText("6자리 이상으로 해주세요.");    // 경고 메세지
+                    txtError2.setTextColor(Color.RED);
+                    edtPw.setBackgroundResource(R.drawable.red_edittext);  // 적색 테두리 적용
+                }
+            }
+        });
+
+        //비밀번호 재입력 오류 시
+        edtPW2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!edtPW2.getText().toString().equals(edtPw.getText().toString())) {
+                    txtError3.setText("비밀번호를 다시 입력해주세요.");    // 경고 메세지
+                    txtError3.setTextColor(Color.RED);
+                    edtPW2.setBackgroundResource(R.drawable.red_edittext);  // 적색 테두리 적용
+                }
+                else{
+                    txtError3.setText("동일합니다.");
+                    txtError3.setTextColor(Color.parseColor("#4CAF50"));
+                    edtPW2.setBackgroundResource(R.drawable.white_edittext);  //테투리 흰색으로 변경
+                }
+            }
+        });
+
+        //회원가입 버튼 클릭
+        btOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
     }
+
+    //최종 회원가입(로그인후 비밀번호 변경)
+    public void register(){
+        final String id = edtID.getText().toString().trim()+"@"+pickEmail.getText().toString().trim();
+        final String adminPW= "adminPW";
+        if(id.length()>0 && adminPW.length()>0){
+            firebaseAuth.signInWithEmailAndPassword(id, adminPW)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                final String pw = edtPw.getText().toString().trim();
+                                user.updatePassword(pw);
+                                Toast.makeText(Register.this, "회원가입을 축하합니다.", Toast.LENGTH_SHORT).show();
+                                firebaseAuth.signOut();
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"아이디 또는 비밀번호를 입력하세요.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"아이디 또는 비밀번호를 입력하세요.(회원가입)",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //뒤로 가기 버튼 클릭 시(계정삭제)
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        final String id = edtID.getText().toString().trim()+"@"+pickEmail.getText().toString().trim();
+        final String adminPW= "adminPW";
+        if(id.length()>0 && adminPW.length()>0){
+            firebaseAuth.signInWithEmailAndPassword(id, adminPW)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        finish();
+                                    }
+                                });
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"뒤로가기 버튼 에러",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"뒤로가기 버튼 에러",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
