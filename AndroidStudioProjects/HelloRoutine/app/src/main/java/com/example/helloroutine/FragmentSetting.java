25288fb35751b333428e8d58a3a5c04a80fcf140
LEE Junho <cse05214@naver.com>
2021-10-11 17:25:17 +0900

    회원탈퇴(카카오)

--- AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/FragmentSetting.java
+++ AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/FragmentSetting.java
@@ -1,5 +1,6 @@
 package com.example.helloroutine;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,34 +11,109 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.kakao.auth.ApiErrorCode;
+import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.UnLinkResponseCallback;
 
 public class FragmentSetting extends Fragment {
 
-    Button btnLogout;
+    Button btnLogout, btnRemove;
+    FirebaseAuth firebaseAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_setting, container, false);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         btnLogout = view.findViewById(R.id.btnLogout);
+        btnRemove = view.findViewById(R.id.btnRemove);
 
         btnLogout.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(), "정상적으로 로그아웃되었습니다.", Toast.LENGTH_SHORT).show();
 
+                //일반 계정 로그아웃
+                firebaseAuth.signOut();
+                Toast.makeText(getActivity().getApplicationContext(), "로그아웃이 되었습니다(1)", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+                getActivity().finish();
+
+                //카카오 로그아웃
                 UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
                     @Override
                     public void onCompleteLogout() {
+                        Toast.makeText(getActivity().getApplicationContext(), "로그아웃되었습니다(2)", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(getActivity(), Login.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent);
+                        getActivity().finish();
                     }
                 });
+            }
+        });
+
+        btnRemove.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getActivity())
+                        .setMessage("탈퇴하시겠습니까?")
+                        .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                UserManagement.getInstance().requestUnlink(new UnLinkResponseCallback() {
+                                    @Override
+                                    public void onFailure(ErrorResult errorResult) {
+                                        int result = errorResult.getErrorCode();
+
+                                        if(result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                                            Toast.makeText(getActivity().getApplicationContext(), "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(getActivity().getApplicationContext(), "회원탈퇴에 실패했습니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onSessionClosed(ErrorResult errorResult) {
+                                        Toast.makeText(getActivity().getApplicationContext(), "로그인 세션이 닫혔습니다. 다시 로그인해 주세요.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                                        startActivity(intent);
+                                        getActivity().finish();
+                                    }
+
+                                    @Override
+                                    public void onNotSignedUp() {
+                                        Toast.makeText(getActivity().getApplicationContext(), "가입되지 않은 계정입니다. 다시 로그인해 주세요.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                                        startActivity(intent);
+                                        getActivity().finish();
+                                    }
+
+                                    @Override
+                                    public void onSuccess(Long result) {
+                                        Toast.makeText(getActivity().getApplicationContext(), "회원탈퇴에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getActivity(), Login.class);
+                                        startActivity(intent);
+                                        //getActivity().finish();
+                                    }
+                                });
+
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).show();
             }
         });
 
--- AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/Login.java
+++ AndroidStudioProjects/HelloRoutine/app/src/main/java/com/example/helloroutine/Login.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.common.SignInButton;
@@ -43,6 +44,8 @@ public class Login extends AppCompatActivity {
     private boolean saveID;
     String sID;
     private SessionCallback sessionCallback;
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long backPressedTime = 0;
 
 
     @Override
@@ -51,11 +54,19 @@ public class Login extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         setContentView(R.layout.login);
+        edtID = findViewById(R.id.loginID);
+        edtPw = findViewById(R.id.loginPW);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
         checkBox = findViewById(R.id.saveLogin);
         btnKakao = findViewById(R.id.btnKakao);
         firebaseAuth = FirebaseAuth.getInstance();
+        
+        //일반 계정 자동 로그인
+        if(firebaseAuth.getCurrentUser() != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
 
         //카카오 로그인 콜백 초기화
         sessionCallback = new SessionCallback();
@@ -63,12 +74,12 @@ public class Login extends AppCompatActivity {
         //앱 실행 시 로그인 토큰이 있으면 자동으로 로그인 수행
         Session.getCurrentSession().checkAndImplicitOpen();
 
-        //로그인 버튼
+
+        //일반 로그인 버튼
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+                login(edtID.getText().toString(), edtPw.getText().toString());
             }
         });
 
@@ -85,10 +96,51 @@ public class Login extends AppCompatActivity {
         btnKakao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
                 Session.getCurrentSession().open(AuthType.KAKAO_LOGIN_ALL, Login.this);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
+            super.onBackPressed();
+            ActivityCompat.finishAffinity(this);
+            System.exit(0);
+        }
+        else {
+            backPressedTime = tempTime;
+            Toast.makeText(this, "뒤로 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    //일반 로그인(파이어베이스)
+    public void login(String id, String pw){
+        id = edtID.getText().toString().trim();
+        pw = edtPw.getText().toString().trim();
+        if(id.length()>0 && pw.length()>0){
+            firebaseAuth.signInWithEmailAndPassword(id, pw)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"아이디 또는 비밀번호가 틀렸습니다.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"아이디 또는 비밀번호를 입력하세요.",Toast.LENGTH_SHORT).show();
+        }
     }
 
     //카카오 로그인
@@ -110,57 +162,54 @@ public class Login extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(), "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
                         finish();
                     } else {
-                        Toast.makeText(getApplicationContext(),"로그인 도중 오류가 발생했습니다: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+
                     }
                 }
-
                 @Override
                 public void onSessionClosed(ErrorResult errorResult) {
-                    Toast.makeText(getApplicationContext(),"세션이 닫혔습니다. 다시 시도해 주세요: "+errorResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
-                }
 
+                }
                 @Override
                 public void onSuccess(MeV2Response result) {
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
 
-                    final String id = "k"+result.getKakaoAccount().getEmail().toString().trim();
+                    final String id = "kk"+result.getKakaoAccount().getEmail().toString().trim();
                     final String adminPW = "adminPW";
                     firebaseAuth.createUserWithEmailAndPassword(id, adminPW).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 Intent intent = new Intent(Login.this, MainActivity.class);
-                                intent.putExtra("name", result.getNickname());
+                                startActivity(intent);
 
+                                //카카오 계정의 이메일과 이름 가져오기
+                                intent.putExtra("name", result.getNickname());
                                 if(result.getKakaoAccount().hasEmail() == OptionalBoolean.TRUE)
                                     intent.putExtra("email", result.getKakaoAccount().getEmail());
                                 else
                                     intent.putExtra("email", "none");
-                                startActivity(intent);
-                                finish();
                             }
                             else {
                                 Intent intent = new Intent(Login.this, MainActivity.class);
-                                intent.putExtra("name", result.getNickname());
+                                startActivity(intent);
 
+                                //카카오 계정의 이메일과 이름 가져오기
+                                intent.putExtra("name", result.getNickname());
                                 if(result.getKakaoAccount().hasEmail() == OptionalBoolean.TRUE)
                                     intent.putExtra("email", result.getKakaoAccount().getEmail());
                                 else
                                     intent.putExtra("email", "none");
-                                startActivity(intent);
-                                finish();
-
                             }
                         }
                     });
-
-
                 }
             });
         }
 
         @Override
         public void onSessionOpenFailed(KakaoException e) {
-            Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: "+e.toString(), Toast.LENGTH_SHORT).show();
+
         }
     }
     @Override
